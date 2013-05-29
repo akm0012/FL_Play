442fec1635acd59c0d4f159a0d0a145024413120
akm0012 <akm0012@auburn.edu>
2013-05-29 13:37:13 -0500

    Middle of Fragment playing.

--- FutureLab_Play/src/edu/auburn/csse/akm0012/futurelab_play/TestFragment.java
+++ FutureLab_Play/src/edu/auburn/csse/akm0012/futurelab_play/TestFragment.java
@@ -1,22 +1,35 @@
 package edu.auburn.csse.akm0012.futurelab_play;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TestFragment extends Fragment {
 	
 	View view;
-	
+		
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
-		
 		view = inflater.inflate(R.layout.activity_side_panel_fragment, container,false);
 		
+		Button comment = (Button) view.findViewById(R.id.commentButton);
+		comment.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				
+			}
+			
+		});
+		
 		return view;
+
 	}
 
 }
